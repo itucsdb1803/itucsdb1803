41dc130a9882560edde27885a163517e99199136
saykara <saykara@itu.edu.tr>
2018-11-06 13:20:18 +0300

    fixed Heroku deployment error

--- database.py
+++ database.py
@@ -9,20 +9,20 @@ class DatabaseOperations:
         DATABASE_URL = os.environ('DATABASE_URL')
 
         if DATABASE_URL is not None:
-            self.config = DatabaseOperations.get_elephantsql_dsn(DATABASE_URL)
+            self.config = DATABASE_URL
         else:
             self.config = """user='postgres' password='12345' host='localhost' port=5432 dbname='itucsdb1803'"""
 
-    @classmethod
-    def get_elephantsql_dsn(cls, vcap_services):
-        """Returns the data source name for ElephantSQL."""
-        parsed = json.loads(vcap_services)
-        uri = parsed["elephantsql"][0]["credentials"]["uri"]
-        match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
-        user, password, host, _, port, dbname = match.groups()
-        dsn = """user='{}' password='{}' host='{}' port={}
-                 dbname='{}'""".format(user, password, host, port, dbname)
-        return dsn
+    # @classmethod
+        #    def get_elephantsql_dsn(cls, vcap_services):
+        #"""Returns the data source name for ElephantSQL."""
+        #parsed = json.loads(vcap_services)
+        #uri = parsed["elephantsql"][0]["credentials"]["uri"]
+        #match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+        #user, password, host, _, port, dbname = match.groups()
+            #dsn = """user='{}' password='{}' host='{}' port={}
+        #    dbname='{}'""".format(user, password, host, port, dbname)
+    #return dsn"""
 
     def create_tables(self):
         with dbapi2.connect(self.config, sslmode='require') as connection:
