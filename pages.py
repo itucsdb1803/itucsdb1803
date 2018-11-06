830c2509ac046ed22d840b0af4b5aeaf984a3553
Orhan Kurto <43881947+kurtorhan@users.noreply.github.com>
2018-11-06 16:35:55 +0300

    created login page and authentication

--- pages.py
+++ pages.py
@@ -1,6 +1,7 @@
-from database import DatabaseOperations
-from flask import Blueprint, render_template
+from werkzeug.utils import redirect
 
+from database import DatabaseOperations
+from flask import Blueprint, render_template, request, url_for
 
 site = Blueprint('site', __name__,)
 
@@ -17,6 +18,12 @@ def home_page():
     return render_template("home.html")
 
 
-@site.route("/login")
+@site.route("/login", methods=['GET', 'POST'])
 def login_page():
-    return "Hello to Login Page!"
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('site.home_page'))
+    return render_template('login.html', error=error)
