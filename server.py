dd3ed2b31606837a4bb4da08e72ce1cb134e3973
saykara <saykara@itu.edu.tr>
2018-11-06 15:02:36 +0300

    created paging structure

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
-import os
 from flask import Flask
 from database import DatabaseOperations
+import pages
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_page():
-    db = DatabaseOperations()
-    db.create_tables()
-    return "Hello World"
+def create_app():
+    app.config.from_object("settings")
+    app.add_url_rule("/initdb", view_func=pages.initialize_database)
+    app.add_url_rule("/", view_func=pages.home_page)
+    app.add_url_rule("/index", view_func=pages.home_page)
+    app.add_url_rule("/login", view_func=pages.login_page)
 
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     port = app.config.get("PORT", 5000)
-    debug = True
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port)
