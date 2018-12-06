bee7fbc02572f25cadc5984dd3b010c5a12157ef
saykara <saykara@itu.edu.tr>
2018-12-06 16:22:03 +0300

    changed blueprint initialization

--- server.py
+++ server.py
@@ -1,23 +1,22 @@
 from flask import Flask
 from pages import site
 from flask_login import LoginManager
+import flask.globals as flask_global
 from login import *
 
 app = Flask(__name__)
-login_manager = LoginManager()
 app.config.from_object('settings')
-app.register_blueprint(site)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "site.login_page"
 
 @login_manager.user_loader
 def load_user(user_id):
     return LoginDatabase.select_login_info(user_id)
 
+app.register_blueprint(site)
+
 if __name__ == '__main__':
-
-
-    login_manager.init_app(app)
-    login_manager.login_view = 'site.login_page'
-
     port = app.config.get("PORT", 5000)
     debug = True
     app.run(host='0.0.0.0', port=port, debug=debug)
