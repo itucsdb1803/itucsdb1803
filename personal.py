fee9ca5ca541b9bda0f4c640e57c46f9b787e321
saykara <saykara@itu.edu.tr>
2018-12-10 20:02:28 +0300

    added update function of personal and personal html

--- personal.py
+++ personal.py
@@ -17,12 +17,45 @@ class Personal(UserMixin):
 
 class PersonalDatabase:
     @classmethod
-    def add_personal(cls, UserID, hospitalid, departmentID, createUserID, userType, regNu, birthPlace, name, surname, birthDay):
+    def add_personal(cls, UserID, hospitalID, departmentID, createUserID, userType, regNu, birthPlace, name, surname, birthDay):
         with dbapi2.connect(database.config) as connection:
             cursor = connection.cursor()
             query = """INSERT INTO PersonalInfo(UserID, HospitalID, DepartmentID, createUserID, UserType, RegNu, BirthPlace, Name, Surname, BirthDay) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
             try:
-                cursor.execute(query, (str(UserID), str(hospitalid), str(departmentID), str(createUserID), str(userType), str(regNu), str(birthPlace), str(name), surname, None))
+                cursor.execute(query, (str(UserID), str(hospitalID), str(departmentID), str(createUserID), str(userType), str(regNu), str(birthPlace), str(name), surname, str(birthDay)))
+            except dbapi2.Error:
+                connection.rollback()
+            else:
+                connection.commit()
+            cursor.close()
+
+    @classmethod
+    def update_personal(cls, userID, hospitalID, departmentID, userType, regNu, birthPlace, name, surname, birthDay):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+
+            query = 'UPDATE PersonalInfo SET UpdateDate = ' + "'" + str(datetime.datetime.now()) + "'"
+
+            if (hospitalID != '' and hospitalID is not None):
+                query = query + ", HospitalID = '" + str(hospitalID) + "'"
+            if (departmentID != '' and departmentID is not None):
+                query = query + ", DepartmentID = '" + str(departmentID) + "'"
+            if (userType != '' and userType is not None):
+                query = query + ", UserType = '" + str(userType) + "'"
+            if (regNu != '' and regNu is not None):
+                query = query + ", RegNu = '" + str(regNu) + "'"
+            if (birthPlace != '' and birthPlace is not None):
+                query = query + ", BirthPlace = '" + str(birthPlace) + "'"
+            if (name != '' and name is not None):
+                query = query + ", Name = '" + str(name) + "'"
+            if (surname != '' and surname is not None):
+                query = query + ", Surname = '" +  str(surname) + "'"
+            if (birthDay != '' and birthDay is not None):
+                query = query + ", BirthDay = '" + str(birthDay) + "'"
+            query = query + ' WHERE UserID = ' + str(userID)
+
+            try:
+                cursor.execute(query)
             except dbapi2.Error:
                 connection.rollback()
             else:
