1e4b46519e816359e963f2049ec9a7f61c34263d
saykara <saykara@itu.edu.tr>
2018-12-15 10:01:50 +0300

    Added patient function

--- patient.py
+++ patient.py
@@ -14,3 +14,19 @@ class Patient(UserMixin):
         self.Surname = surname
         self.BirthDay = birthDay
         self.UpdateDate = updateDate
+
+
+class PatientDatabase:
+    @classmethod
+    def add_patient(cls, patientId, tckn, createUserID, birthPlace, gsm, eMail, name, surname, birthDay):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+            query = """INSERT INTO PatientInfo(PatientId, CreateUserID, TCKN, BirthPlace, GSM, EMail, Name, Surname, BirthDay) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            try:
+                cursor.execute(query, (str(patientId), str(createUserID), str(tckn), str(birthPlace), str(gsm), str(eMail), str(name), str(name), str(surname), str(birthDay)))
+            except dbapi2.Error:
+                connection.rollback()
+            else:
+                connection.commit()
+            cursor.close()
+            return
