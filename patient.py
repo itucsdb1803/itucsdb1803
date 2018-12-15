af06988cbda7d7ac99f10b13d9bf914e6f5d50ea
saykara <saykara@itu.edu.tr>
2018-12-15 10:12:40 +0300

    Added patient update function

--- patient.py
+++ patient.py
@@ -30,3 +30,35 @@ class PatientDatabase:
                 connection.commit()
             cursor.close()
             return
+
+    @classmethod
+    def update_patient(cls, patientId, tckn, birthPlace, gsm, eMail, name, surname, birthDay):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+
+            query = 'UPDATE PatientInfo SET UpdateDate = ' + "'" + str(datetime.datetime.now()) + "'"
+
+            if eMail != '' and eMail is not None:
+                query = query + ", EMail = '" + str(eMail) + "'"
+            if gsm != '' and gsm is not None:
+                query = query + ", GSM = '" + str(gsm) + "'"
+            if tckn != '' and tckn is not None:
+                query = query + ", TCKN = '" + str(tckn) + "'"
+            if birthPlace != '' and birthPlace is not None:
+                query = query + ", BirthPlace = '" + str(birthPlace) + "'"
+            if name != '' and name is not None:
+                query = query + ", Name = '" + str(name) + "'"
+            if surname != '' and surname is not None:
+                query = query + ", Surname = '" + str(surname) + "'"
+            if birthDay != '' and birthDay is not None:
+                query = query + ", BirthDay = '" + str(birthDay) + "'"
+            query = query + ' WHERE PatientId = ' + str(patientId)
+
+            try:
+                cursor.execute(query)
+            except dbapi2.Error:
+                connection.rollback()
+            else:
+                connection.commit()
+            cursor.close()
+            return
